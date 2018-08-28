c9acdf71e5e1be23e23c1dac9dd87e71a58c90bf
Zhorander <elias_50@live.com>
2018-08-28 16:19:12 -0600

    moved asset folder to src; added basic menu module

---
+++ src/main.lua
@@ -0,0 +1,20 @@
+-- main.lua
+-- entry point of the game
+
+local menu = require('menu.menu')
+
+function love.load()
+    font = love.graphics.newFont('assets/itlivesintheswamp.ttf', 50)
+    love.graphics.setFont(font)
+    love.graphics.setBackgroundColor(255,25,255)
+    menu.load()
+    --other.load()
+end
+
+function love.update()
+    menu.update()
+end
+
+function love.draw()
+    menu.draw()
+end
---
+++ src/menu/menu.lua
@@ -0,0 +1,29 @@
+local menu = {}
+
+local width = love.graphics.getWidth()
+local height = love.graphics.getHeight()
+
+
+function menu.load()
+    -- love.graphics.setFont(font)
+end
+
+function menu.draw()
+    love.graphics.setColor(0, 255, 153)
+
+    local rect_x = width * .2
+    local rect_y = height * .1
+
+    love.graphics.rectangle('fill', rect_x, rect_y, .6 * width, .2 * height)
+
+    love.graphics.setColor(0,0,0)
+    love.graphics.printf('LIT FAM', rect_x * 2, rect_y * 1.7, 200, 'center')
+
+end
+
+function menu.update()
+    width = love.graphics.getWidth()
+    height = love.graphics.getHeight()
+end
+
+return menu
