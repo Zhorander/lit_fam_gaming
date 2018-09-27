3bdcbde583d2d978ed5b0b8c28aa2c4ab4b7b4e3
Strider102 <35611073+Strider102@users.noreply.github.com>
2018-09-27 10:55:07 -0600

    map.lua added
    yote

--- src/levels/test/test.lua
+++ src/levels/test/test.lua
@@ -1,43 +1,16 @@
--- small demo for TiledMap loader
---love.filesystem.load("tiledmap.lua")()
-tileMap = require('loader.loader') 
+local sti = require "sti"
 
-local testLevel = {}
-
-gKeyPressed = {}
-gCamX,gCamY = 100,100
- 
-function testLevel.load()
-	TiledMap_Load("assets/map/test.tmx")
-end
- 
-function testLevel.keyreleased( key )
-	gKeyPressed[key] = nil
-end
- 
-function testLevel.keypressed( key, unicode ) 
-	gKeyPressed[key] = true 
-	if (key == "escape") then os.exit(0) end
-	-- if (key == " ") then -- space = next map
-	-- 	gMapNum = (gMapNum or 1) + 1
-	-- 	if (gMapNum > 10) then gMapNum = 1 end
-	-- 	TiledMap_Load(string.format("assets/map/test.tmx",gMapNum))
-	-- 	gCamX,gCamY = 100,100
-	-- end
-end
- 
-function testLevel.update( dt )
-	local s = 500*dt
-	if (gKeyPressed.up) then gCamY = gCamY - s end
-	if (gKeyPressed.down) then gCamY = gCamY + s end
-	if (gKeyPressed.left) then gCamX = gCamX - s end
-	if (gKeyPressed.right) then gCamX = gCamX + s end
-end
- 
-function testLevel.draw()
-    love.graphics.print('arrow-keys=scroll, space=next map', 50, 50)
-	love.graphics.setBackgroundColor(0x80,0x80,0x80)
-	TiledMap_DrawNearCam(gCamX,gCamY)
+function love.load()
+    -- Load map file
+    map = sti("assets.map.test.lua")
 end
 
-return testLevel
+function love.update(dt)
+    -- Update world
+    map:update(dt)
+end
+
+function love.draw()
+    -- Draw world
+    map:draw()
+end
