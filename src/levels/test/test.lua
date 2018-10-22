f86df59199caff98d14b6b71c7269e4174d48afa
zhorander <elias_50@live.com>
2018-10-22 12:04:50 -0600

    added character class n stuff

--- src/levels/test/test.lua
+++ src/levels/test/test.lua
@@ -1,9 +1,15 @@
+local Character = require("util.character")
+
 local sti = require "sti"
 local test = {}
 
 function test.load()
     -- Load map file
     map = sti("assets/map/temp.lua")
+
+    --load character
+    character = Character:new(nil, 0, 'assets/RoguePlayer.png', 32)
+    print('<><><><><><><><><',character)
 end
 
 function test.update(dt)
@@ -14,6 +20,9 @@ end
 function test.draw()
     -- Draw world
     map:draw()
+
+    --draw player
+    character:draw()
 end
 
 return test
---
+++ src/util/character.lua
@@ -0,0 +1,43 @@
+local Character = {}
+
+function Character:new(child, id, path, sprite_size)
+    local child = child or {}
+
+    child = setmetatable(child, self)
+    self.__index = self
+    child.id = id
+    child.path = path
+    child.x = 0
+    child.y = 0
+    child.sprite_size = sprite_size
+    child.image = love.graphics.newImage(path)
+    child.quad = love.graphics.newQuad(0, 0, sprite_size, sprite_size, child.image:getDimensions())
+
+    return path
+end
+
+-- set the quadrant position for the sprite
+-- perhaps we need to make an animation class instead?
+function Character:setQuad(x, y)
+    self.quad =
+    love.graphics.newQuad(x, y, self.sprite_size, self.sprite_size, self.image:getDimensions())
+end
+
+function Character:setPos(x, y)
+    child.x = x
+    child.y = y
+end
+
+function Character:getId()
+    return self.id
+end
+
+function Character:setId(id)
+    self.id = id
+end
+
+function Character:draw()
+    love.graphics.draw(image, character, self.x, self.y)
+end
+
+return Character
