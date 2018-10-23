febb0b48d06629bae02e5fffbcbb1e7d6029f1b8
Strider102 <35611073+Strider102@users.noreply.github.com>
2018-10-23 11:20:42 -0600

    movement?

--- src/levels/test/test.lua
+++ src/levels/test/test.lua
@@ -3,7 +3,8 @@ local Character = require("util.character")
 local sti = require "sti"
 local test = {}
 
-local step = 5
+local step = 100
+local delta = 0.1
 
 function test.load()
     -- Load map file
@@ -16,6 +17,23 @@ end
 function test.update(dt)
     -- Update world
     map:update(dt)
+
+    if love.keyboard.isDown('d') then
+        delta = step * dt
+        test.keypressed('d')
+    end
+    if love.keyboard.isDown('w') then
+        delta = step * dt
+        test.keypressed('w')
+    end
+    if love.keyboard.isDown('s') then
+        delta = step * dt
+        test.keypressed('s')
+    end
+    if love.keyboard.isDown('a') then
+        delta = step * dt
+        test.keypressed('a')
+    end
 end
 
 function test.draw()
@@ -29,19 +47,19 @@ end
 function test.keypressed(k)
     if k == 'd' then
         local x, y = character:getPos()
-        character:setPos(x + step, y)
+        character:setPos(x + delta, y)
     end
     if k == 'a' then
         local x, y = character:getPos()
-        character:setPos(x - step, y)
+        character:setPos(x - delta, y)
     end
     if k == 's' then
         local x, y = character:getPos()
-        character:setPos(x, y + step)
+        character:setPos(x, y + delta)
     end
     if k == 'w' then
         local x, y = character:getPos()
-        character:setPos(x, y - step)
+        character:setPos(x, y - delta)
     end
 end
 
